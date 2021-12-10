b8ab9216503e5ae3c563e2a4d9179c58a948195a
이진유 <22yuu@naver.com>
2021-12-11 00:38:00 +0900

    2.5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true, minlenght: 10 })`
-  background-color: tomato;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
 `;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+
+  ${Emoji}:hover {
+      font-size: 96px;
+    }
+  }
+`;
+
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper as="header">
+      <Box>
+        <Emoji as="p">🤣</Emoji>
+      </Box>
+      <Emoji>🤟</Emoji>
+    </Wrapper>
   );
 }
 
