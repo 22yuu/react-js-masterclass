539bd7d7a951dcb656f99bc4f8ce2414f90f79f8
이진유 <22yuu@naver.com>
2021-12-14 00:54:41 +0900

    4.1

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#2d3436",
+  textColor: "#f5f6fa",
+  accentColor: "#2ecc71",
 };
