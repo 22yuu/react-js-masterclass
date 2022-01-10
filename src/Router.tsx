f24f3130e235c5482b4bf7d54ab462e1e08fd0ce
이진유 <22yuu@naver.com>
2022-01-11 00:41:26 +0900

    4.7 Nested Routes Part one

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,19 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/:coinId/*" element={<Coin />} />
+        {/* v6부터 자식 라우터를 사용할려면 /*를 붙여야한다. */}
+        {/*
+            아니면 아래의 코드처럼 사용 가능하다. 
+            <Route path="/:coinId" element={<Coin />}>
+              <Route path="chart" element={<Chart />} /> 
+              <Route path="price" element={<Price />} />
+            <Route>
+            
+            * Chart, Price import 해줘야함
+
+            https://reactrouter.com/docs/en/v6/getting-started/overview 참고
+          */}
         <Route path="/" element={<Coins />} />
       </Routes>
     </BrowserRouter>
